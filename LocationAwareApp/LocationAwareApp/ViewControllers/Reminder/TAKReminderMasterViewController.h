4cfd84f88da8d1c825d788ead7f7888a69946615
Toni Antero Karttunen <toni.antero.karttunen@gmail.com>
2013-03-26 02:11:17 +0200

    Add an isRegionMonitoringActive property to the AppDelegate class. Add TAKReminderMasterViewController.

---
+++ LocationAwareApp/LocationAwareApp/ViewControllers/Reminder/TAKReminderMasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  TAKReminderMasterViewController.h
+//  LocationAwareApp
+//
+//  Created by Toni Antero Karttunen on 3/25/13.
+//  Copyright (c) 2013 Toni Antero Karttunen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TAKReminderMasterViewController : UITableViewController
+
+@end

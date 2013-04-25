39641bb41d36abf02e265b8803257f8ca57c8e01
Toni Antero Karttunen <toni.antero.karttunen@gmail.com>
2013-04-25 21:34:44 +0300

    Minor changes.

---
+++ LocationAwareApp/LocationAwareApp/APIConstants.h
@@ -0,0 +1,19 @@
+//
+//  APIConstants.h
+//  LocationAwareApp
+//
+//  Created by Toni Antero Karttunen on 4/5/13.
+//  Copyright (c) 2013 Toni Antero Karttunen. All rights reserved.
+//
+
+#ifndef LocationAwareApp_APIConstants_h
+#define LocationAwareApp_APIConstants_h
+
+#define TAK_FOURSQUARE_API_KEY             @"ADD YOUR FOURSQUARE API KEY HERE"
+#define TAK_FOURSQUARE_API_SECRET          @"ADD YOUR FOURSQUARE API SECRET HERE"
+#define TAK_FOURSQUARE_API_REDIRECT_URL    @"tonikarttunen://com.tonikarttunen.LocationAwareApp"
+
+#define TAK_GOOGLE_MAPS_API_KEY            @"ADD YOUR GOOGLE MAPS API KEY HERE"
+#define TAK_GOOGLE_PLACES_API_KEY          @"ADD YOUR GOOGLE PLACES API KEY HERE"
+
+#endif
